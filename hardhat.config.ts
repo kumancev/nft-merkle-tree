84fcb913be53fc402dfb8f6161364f5883a90d58
kumancev <kumancev1@gmail.com>
2022-08-08 02:32:12 +0300

    add project files

---
+++ hardhat.config.ts
@@ -0,0 +1,24 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.9",
+  networks: {
+    hardhat: {
+    },
+    goerli: {
+      url: process.env.INFURA,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      gasPrice: 1000
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
+};
+
+export default config;
